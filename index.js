f123eeff8576f98f24cfb9dcd510e42d9a48773f
abalospiolo81-wq <abalospiolo81@gmail.com>
2026-01-29 06:16:44 +0800

    Enhance command loading functionality
    Refactor command loading to support nested directories.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const { Telegraf } = require("telegraf");
 const fs = require("fs");
+const path = require("path");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// AUTO LOAD COMMAND FILES
-fs.readdirSync("./commands").forEach((file) => {
-  if (file.endsWith(".js")) {
-    require(`./commands/${file}`)(bot);
-  }
-});
+function loadCommands(dir) {
+  fs.readdirSync(dir).forEach(file => {
+    const full = path.join(dir, file);
+    if (fs.statSync(full).isDirectory()) {
+      loadCommands(full);
+    } else if (file.endsWith(".js")) {
+      require(full)(bot);
+    }
+  });
+}
+
+loadCommands("./commands");
 
 bot.launch();
 console.log("Bot started");
