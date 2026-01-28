dc7e62bb5be08dbcc60cfd31126b46225c18d141
abalospiolo81-wq <abalospiolo81@gmail.com>
2026-01-29 05:36:22 +0800

    Test

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// BASIC TEST COMMANDS
 bot.start(ctx => ctx.reply("🔥 Bot LIVE!"));
-bot.help(ctx => ctx.reply("✅ Help working"));
-bot.command("ping", ctx => ctx.reply("🏓 Pong"));
+bot.help(ctx => ctx.reply("📖 Help is working"));
+bot.command("ping", ctx => ctx.reply("🏓 Pong!"));
 
+// IMPORTANT
 bot.launch();
 console.log("Bot started");
